602fa505515d7c8506f328a835fe4e0b7fedb867
Kunal Kumar <kunalwithkumar@gmail.com>
2022-07-16 04:55:27 +0000

    adding the api for image and added the microservices for sending msg From master into main

--- src/main/java/com/wipro/doconnect/service/IUserService.java
+++ src/main/java/com/wipro/doconnect/service/IUserService.java
@@ -1,5 +1,7 @@
 package com.wipro.doconnect.service;
 
+import java.io.IOException;
+
 /**
 *
 *@author kunal
@@ -9,11 +11,16 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.wipro.doconnect.dto.AskQuestionDTO;
 import com.wipro.doconnect.dto.PostAnswerDTO;
 import com.wipro.doconnect.entity.Answer;
+import com.wipro.doconnect.entity.ImageModel;
 import com.wipro.doconnect.entity.Question;
 import com.wipro.doconnect.entity.User;
+import com.wipro.doconnect.vo.Message;
 
 public interface IUserService {
 
@@ -32,5 +39,11 @@ public interface IUserService {
 	public List<Answer> getAnswers(Long questionId);
 
 	public List<Question> getQuestions(String topic);
+
+	public BodyBuilder uplaodImage(MultipartFile file) throws IOException;
+
+	public ImageModel getImage(String imageName);
+
+	public Message sendMessage(@Valid Message message);
 
 }
